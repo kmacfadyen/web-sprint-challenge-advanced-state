76de12aa13a8c10958c34b042186d3a15280db56
Kevin MacFadyen <kmacfadyen3@gmail.com>
2023-03-19 22:28:37 -0400

    state

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,39 +1,92 @@
+import { MOVE_CLOCKWISE, MOVE_COUNTERCLOCKWISE, SET_QUIZ_INTO_STATE, SET_SELECTED_ANSWER, SET_INFO_MESSAGE, INPUT_CHANGE, RESET_FORM } from './action-types';
+import axios from 'axios';
+
 // ❗ You don't need to add extra action creators to achieve MVP
-export function moveClockwise() { }
+export function moveClockwise() {
+  return {
+    type: MOVE_CLOCKWISE,
+    payload: 1
+  }
+ }
 
-export function moveCounterClockwise() { }
+export function moveCounterClockwise() {
+  return {
+    type: MOVE_COUNTERCLOCKWISE,
+    payload: -1
+  }
+ }
 
-export function selectAnswer() { }
+export function selectAnswer(inputed) {
+  return {
+    type: SET_SELECTED_ANSWER,
+    payload: inputed
+  }
+ }
 
-export function setMessage() { }
+export function setMessage(message) {
+  return {
+    type: SET_INFO_MESSAGE,
+    payload: message
+  }
+ }
 
-export function setQuiz() { }
+export function setQuiz(currentQuiz) {
+  return {
+    type: SET_QUIZ_INTO_STATE,
+    payload: currentQuiz
+  }
+ }
 
-export function inputChange() { }
+export function inputChange(value, value2) {
+  return {
+    type: INPUT_CHANGE,
+    payload: value, value2
+  }
+ }
 
-export function resetForm() { }
+export function resetForm() {
+  return {
+    type: RESET_FORM
+  }
+ }
 
 // ❗ Async action creators
-export function fetchQuiz() {
+export function fetchQuiz(quiz) {
   return function (dispatch) {
     // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
     // On successful GET:
     // - Dispatch an action to send the obtained quiz to its state
+    dispatch({ type: SET_QUIZ_INTO_STATE, payload: null });
+    axios.get(`http://localhost:9000/api/quiz/next`)
+    .then (res => {
+      dispatch({ type: SET_QUIZ_INTO_STATE, payload: res.data })
+    })
+    .catch(err => console.error(err))
   }
 }
-export function postAnswer() {
+export function postAnswer(data) {
   return function (dispatch) {
     // On successful POST:
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
     // - Dispatch the fetching of the next quiz
+    dispatch({ type: SET_SELECTED_ANSWER, payload: null });
+    axios.post(`http://localhost:9000/api/quiz/answer`, data)
+    .then (res => {
+      dispatch({ type: SET_INFO_MESSAGE, payload: res.data.message })
+    })
+    .catch(err => console.error(err))
   }
 }
-export function postQuiz() {
+export function postQuiz(data) {
   return function (dispatch) {
     // On successful POST:
     // - Dispatch the correct message to the the appropriate state
     // - Dispatch the resetting of the form
+    axios.post(`http://localhost:9000/api/quiz/new`, data)
+    .then (res => {
+      dispatch({ type: SET_INFO_MESSAGE, payload: `Congrats "${res.data.question}" is a great question!`})
+    })
   }
 }
 // ❗ On promise rejections, use log statements or breakpoints, and put an appropriate error message in state
--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,23 +1,67 @@
 // ❗ You don't need to add extra reducers to achieve MVP
 import { combineReducers } from 'redux'
+import { MOVE_CLOCKWISE, MOVE_COUNTERCLOCKWISE, SET_QUIZ_INTO_STATE, SET_SELECTED_ANSWER, SET_INFO_MESSAGE, INPUT_CHANGE, RESET_FORM } from './action-types';
 
 const initialWheelState = 0
 function wheel(state = initialWheelState, action) {
-  return state
+  switch(action.type) {
+    case MOVE_COUNTERCLOCKWISE: {
+      if (state === 0) {
+        return state + 5;
+      }
+      else return state - action.payload;
+    }
+    case MOVE_CLOCKWISE: {
+      if (state === 5) {
+        return initialWheelState;
+      }
+      else return state + action.payload;
+    }
+  default: return state;
+  }
 }
 
-const initialQuizState = null
+const initialQuizState = {
+  currentQuizState: null
+}
+
 function quiz(state = initialQuizState, action) {
-  return state
+  switch(action.type) {
+    case SET_QUIZ_INTO_STATE:
+      return {
+        ...state,
+        currentQuizState: action.payload
+      }
+  }
+  return state;
 }
 
-const initialSelectedAnswerState = null
+const initialSelectedAnswerState = {
+  currentSelectedAnswer: null
+}
 function selectedAnswer(state = initialSelectedAnswerState, action) {
+  switch(action.type) {
+    case SET_SELECTED_ANSWER:
+      return {
+        ...state,
+        currentSelectedAnswer: action.payload
+      }
+  }
   return state
 }
 
-const initialMessageState = ''
+const initialMessageState = {
+  currentMessage: ''
+}
+
 function infoMessage(state = initialMessageState, action) {
+  switch(action.type) {
+    case SET_INFO_MESSAGE:
+      return {
+        ...state,
+        currentMessage: action.payload
+      }
+  }
   return state
 }
 
@@ -26,7 +70,23 @@ const initialFormState = {
   newTrueAnswer: '',
   newFalseAnswer: '',
 }
+
 function form(state = initialFormState, action) {
+  switch(action.type) {
+    case INPUT_CHANGE:
+      return {
+        ...state,
+        ...action.payload
+      }
+
+    case RESET_FORM:
+      return{
+        ...state,
+        newQuestion: '',
+        newTrueAnswer: '',
+        newFalseAnswer: '',
+      }
+  }
   return state
 }
 
